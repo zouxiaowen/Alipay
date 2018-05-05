ba65e9bcaba8585f6a93ece7eda3a1e96fc2e559
zouxiaowen <251687xiao>
2018-05-05 14:23:37 +0800

    微信本地加签

---
+++ app/src/main/java/com/sj/huakr/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.sj.huakr.utils;
+
+public class Constants {
+    // 应用id
+    public static final String APP_ID = "wx7d8371c3e46f3c9d";
+
+    // 商户号
+    public static final String MCH_ID = "1319350901";
+
+    // 秘钥
+    public static final String API_KEY = "f8g327muCXGjV7qG2znvlspLx2Qw068m";
+}
